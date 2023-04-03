de29bf68038e64514aab22b5d9dca219e0181a63
corinB <gkdisrha2020@gmail.com>
2023-04-03 19:44:40 +0900

    qor_test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application. gdgdgdgd
+  // This widget is the root of your application. gdgdgdgd1321321321
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
