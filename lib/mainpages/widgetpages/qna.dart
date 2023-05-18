8c65e00e6bf86f359612f6ab40958e4218694ec2
corinB <gkdisrha2020@gmail.com>
2023-05-18 22:29:45 +0900

    QNA 페이지 buildQNT() 함수 완성

--- lib/mainpages/widgetpages/qna.dart
+++ lib/mainpages/widgetpages/qna.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class QnA extends StatefulWidget {
   @override
@@ -41,6 +42,101 @@ class _QnA extends State<QnA> {
         backgroundColor: Color.fromARGB(255, 249, 248, 253),
         elevation: 0.0,
       ),
+      body: QA(),
     );
   }
+}
+
+class QA extends StatelessWidget {
+  const QA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    final appheight = MediaQuery.of(context).size.height;
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        buildQAT(
+            "질문",
+            "대답",
+            Icon(
+              Icons.access_alarm,
+              size: 50,
+            ),
+            Icon(
+              Icons.access_alarm,
+              size: 50,
+            ),
+            context),
+        buildQAT(
+            "질문",
+            "대답",
+            Icon(
+              Icons.access_alarm,
+              size: 50,
+            ),
+            Icon(
+              Icons.access_alarm,
+              size: 50,
+            ),
+            context),
+        buildQAT(
+            "질문",
+            "대답",
+            Icon(
+              Icons.access_alarm,
+              size: 50,
+            ),
+            Icon(
+              Icons.access_alarm,
+              size: 50,
+            ),
+            context)
+      ],
+    );
+  }
+}
+
+Widget buildQAT(String q, String a, Icon fi, Icon si, BuildContext context) {
+  final appheight = MediaQuery.of(context).size.height;
+  final appwidth = MediaQuery.of(context).size.width;
+  return ListTile(
+    leading: fi,
+    title: SizedBox(
+      height: appheight * 0.09,
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          q,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    ),
+    trailing: si,
+    onTap: () {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            content: Text(
+              a,
+              style: TextStyle(fontSize: 20),
+            ),
+            actions: [
+              Center(
+                child: TextButton(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    },
+  );
 }
